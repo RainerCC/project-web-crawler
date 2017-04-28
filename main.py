16b3c96a4eb7469a86346ad0e72731a8e51c7f6e
Rainer CC <rainer_cc@outlook.com>
2017-04-28 19:16:51 +0800

    Add get page from url function

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import codecs
+import urllib.request
+
+def get_page(url):
+	try:
+		return urllib.request.urlopen(url).read().decode('utf-8')
+	except:
+		return ""
 
 def get_next_target(page):
-    start_link = page.find('<a href=')
+	start_link = page.find('<a href=')
 
-    #Insert your code below here
-    if(start_link == -1):
-        return None, 0
+	#Insert your code below here
+	if(start_link == -1):
+		return None, 0
     
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1:end_quote]
-    return url, end_quote
+	start_quote = page.find('"', start_link)
+	end_quote = page.find('"', start_quote + 1)
+	url = page[start_quote + 1:end_quote]
+	return url, end_quote
                  
 def get_all_links(page):
-    links = []
-    while True:
-        url, endpos = get_next_target(page)
-        if url:
-            links.append(url)
-            page = page[endpos:]
-        else:
-            return links
+	links = []
+	while True:
+		url, endpos = get_next_target(page)
+		if url:
+			links.append(url)
+			page = page[endpos:]
+		else:
+			return links
         
 
+"""
 f = codecs.open('sample-page.txt', encoding='utf-8')
-
 page = f.read()
-
 f.close()
+"""
+
+page = get_page("http://www.wikipedia.org/")
 
 print (get_all_links(page))
